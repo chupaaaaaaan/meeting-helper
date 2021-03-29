7275309285e795c840a9a91333844127137b9e7f
Takayuki Uchida <jacobitebeerlove@gmail.com>
2021-03-29 13:43:30 +0900

    htmlとjsを分離

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const http = require("http");
 const fs = require("fs");
-http
-    .createServer((req,res) => {
-        console.log(req.url);
-        const html = fs.readFileSync("./index.html");
-        res.writeHeader(200, { "Content-Type": "text/html" });
-        res.write(html);
-        res.end();
-    })
-    .listen(3000);
+
+
+var server = http.createServer();
+
+
+server.on('request',(req, res) => {
+    console.log(req.url);
+    var html;
+
+    switch (req.url) {
+    case '/elm.js':
+        html = fs.readFileSync("./elm.js");
+        res.writeHeader(200, {'Content-Type' : 'text/javascript'});
+        break;
+
+    default:
+        html = fs.readFileSync("./index.html");
+        res.writeHeader(200, {'Content-Type' : 'text/html'});
+        break;
+    }
+
+    res.write(html);
+    res.end();
+
+});
+
+server.listen(3000);
